ec4252acbd1c87bc7a67af40fc145f6cce419d22
JennyLuo8 <jenny.luo@outlook.com>
2014-11-15 19:17:54 -0600

    Revert "Revert "First commit of webapp code""
    This reverts commit 707d341caa4e07710e534d90f3a65d3cf12d8210.

---
+++ FindTheTime/src/ee461l/CreateAccountServlet.java
@@ -0,0 +1,21 @@
+package ee461l;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class CreateAccountServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		
+		resp.sendRedirect("/myAccount.jsp");
+	}
+}
